200c56ad749a0ad2e3f23cb98f72451f7a2bb820
gellel <lindsay.gelle@gmail.com>
2019-06-20 00:36:20 +1000

    Editing slice string interface

--- string.go
+++ string.go
@@ -3,3 +3,24 @@ package slice
 type String struct {
 	slice *Slice
 }
+
+func (pointer *String) Append(s string) *String {
+	pointer.slice.Append(s)
+	return pointer
+}
+
+func (pointer *String) Assign(s ...string) *String {
+	for i := range s {
+		pointer.Append(s[i])
+	}
+	return pointer
+}
+
+func (pointer *String) Bounds(i int) bool {
+	return pointer.slice.Bounds(i)
+}
+
+func (pointer *String) Concatenate(s *String) *String {
+	pointer.slice.Concatenate(s.slice)
+	return pointer
+}
