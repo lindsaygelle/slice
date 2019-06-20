7281c56e0eb5b0b70be9470a704363c4784f3909
gellel <lindsay.gelle@gmail.com>
2019-06-20 11:29:02 +1000

    Adding tests

--- slice_test.go
+++ slice_test.go
@@ -1,6 +1,7 @@
 package slice_test
 
 import (
+	"math/rand"
 	"reflect"
 	"testing"
 
@@ -76,5 +77,44 @@ func TestConcatenate(t *testing.T) {
 
 	if i != "x" {
 		t.Fatalf("slice.Concatenate(s *Slice) did not append the contents of a sibling slice to the reference slice")
+	}
+}
+
+func TestEach(t *testing.T) {
+
+	currentLength := s.Len()
+
+	i := 0
+
+	s.Each(func(j int, value interface{}) {
+		if (*s)[i] != value {
+			t.Fatalf("slice.Each(func(i int, value interface{}) value != (*s)[i]")
+		}
+		i = i + 1
+	})
+
+	if i != currentLength {
+		t.Fatalf("slice.Each(func(i int, value interface{}) final i count is not equal to slice length")
+	}
+}
+
+func TestFetch(t *testing.T) {
+
+	i := rand.Intn(s.Len() - 1)
+
+	if s.Fetch(i) == nil {
+		t.Fatalf("slice.Fetch(i int) did not return a non-nil property")
+	}
+}
+
+func TestGet(t *testing.T) {
+
+	i := rand.Intn(s.Len() - 1)
+
+	if _, ok := s.Get(i); ok != true {
+		t.Fatalf("slice.Get(i int) did not return an interface and a true boolean")
+	}
+	if i, ok := s.Get(-1); ok != false || i != nil {
+		t.Fatalf("slice.Get(i int) did not return nil or a false boolean")
 	}
 }
