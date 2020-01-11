906a7a530234f33431838a670101b0ca8af750c2
gellel <lindsay.gelle@gmail.com>
2020-01-11 18:31:07 +1100

    Adding content to package

--- slice_test.go
+++ slice_test.go
@@ -6,6 +6,23 @@ import (
 	"github.com/gellel/slice"
 )
 
+var (
+	b   slice.Byter
+	f32 slice.Floater32
+	f64 slice.Floater64
+	i   slice.Inter
+	i8  slice.Inter8
+	i16 slice.Inter16
+	i32 slice.Inter32
+	i64 slice.Inter64
+	u   slice.UInter
+	u8  slice.UInter8
+	u16 slice.UInter16
+	u32 slice.UInter32
+	u64 slice.UInter64
+	v   slice.Interfacer
+)
+
 func Test(t *testing.T) {
 
 	var (
