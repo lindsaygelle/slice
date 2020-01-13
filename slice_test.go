3f52fd86f81fd955f880589a9b4cfa6e69e2ba04
gellel <lindsay.gelle@gmail.com>
2020-01-13 23:09:51 +1100

    Adding unit tests

--- slice_test.go
+++ slice_test.go
@@ -15,6 +15,7 @@ var (
 	i16 slice.Inter16
 	i32 slice.Inter32
 	i64 slice.Inter64
+	s   *slice.Slice
 	u   slice.UInter
 	u8  slice.UInter8
 	u16 slice.UInter16
@@ -24,9 +25,56 @@ var (
 )
 
 func Test(t *testing.T) {
+	s = &slice.Slice{}
+}
 
+func TestAppend(t *testing.T) {
+	if ok := (*s.Append("a"))[0].(string) == "a"; !ok {
+		t.Fatalf("(&slice.Slice.Append(interface{})) != (interface{}))")
+	}
+}
+
+func TestBounds(t *testing.T) {
+	if ok := s.Bounds(s.Len() - 1); !ok {
+		t.Fatalf("(&slice.Slice.Bounds(int) bool) != true")
+	}
+}
+
+func TestConcatenate(t *testing.T) {
+	if ok := (*s.Concatenate(&slice.Slice{"b"}))[1].(string) == "b"; !ok {
+		t.Fatalf("(&slice.Slice.Concatenate(interface{})) != (interface{}))")
+	}
+}
+
+func TestEach(t *testing.T) {
+	s.Each(func(i int, v interface{}) {
+		if ok := (*s)[i] == v; !ok {
+			t.Fatalf("(&slice.Slice.Each(int, interface{})) != (interface{})")
+		}
+	})
+}
+
+func TestEachBreak(t *testing.T) {
 	var (
-		s = slice.NewStringer()
+		n int
 	)
-	s.Bounds(0)
+	s.EachBreak(func(i int, _ interface{}) bool {
+		n = i
+		return false
+	})
+	if ok := n == 0; !ok {
+		t.Fatalf("(&slice.Slice.EachBreak(int, interface{}) bool) != true")
+	}
+}
+
+func TestEachReverse(t *testing.T) {
+	var (
+		n = s.Len() - 1
+	)
+	s.EachReverse(func(i int, _ interface{}) {
+		if ok := i == n; !ok {
+			t.Fatalf("(&slice.Slice.EachReverse(i int, interface{})) > s.Len() - i")
+		}
+		n = n - i
+	})
 }
