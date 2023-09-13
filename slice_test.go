d8e3786d14478433ac4c7c3d4bc5e8e74a5182c4
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-09-13 14:56:27 +1000

    Update slice_test.go

--- slice_test.go
+++ slice_test.go
@@ -6,40 +6,16 @@ import (
 	"github.com/lindsaygelle/slice"
 )
 
-func TestAppend(t *testing.T)            {}
-func TestAppendLength(t *testing.T)      {}
-func TestBounds(t *testing.T)            {}
-func TestConcatenate(t *testing.T)       {}
-func TestConcatenateLength(t *testing.T) {}
-func TestDelete(t *testing.T)            {}
-func TestDeleteLength(t *testing.T)      {}
-func TestEach(t *testing.T)              {}
-func TestEachBreak(t *testing.T)         {}
-func TestEachReverse(t *testing.T)       {}
-func TestEachReverseBreak(t *testing.T)  {}
-func TestFetch(t *testing.T)             {}
-func TestFetchLength(t *testing.T)       {}
-func TestGet(t *testing.T)               {}
-func TestGetLength(t *testing.T)         {}
-func TestLen(t *testing.T)               {}
-func TestMake(t *testing.T)              {}
-func TestMakeEach(t *testing.T)          {}
-func TestMakeEachReverse(t *testing.T)   {}
-func TestMap(t *testing.T)               {}
-func TestPoll(t *testing.T)              {}
-func TestPollLength(t *testing.T)        {}
-func TestPollOK(t *testing.T)            {}
-func TestPop(t *testing.T)               {}
-func TestPopLength(t *testing.T)         {}
-func TestPopOK(t *testing.T)             {}
-func TestPrecatenate(t *testing.T)       {}
-func TestPrecatenateLength(t *testing.T) {}
-func TestPrepend(t *testing.T)           {}
-func TestPrependLength(t *testing.T)     {}
-func TestPush(t *testing.T)              {}
-func TestReplace(t *testing.T)           {}
-func TestReverse(t *testing.T)           {}
-func TestSet(t *testing.T)               {}
-func TestSlice(t *testing.T)             {}
-func TestSwap(t *testing.T)              {}
-func TestUnshift(t *testing.T)           {}
+// TestAppend tests Slice.Append.
+func TestAppend(t *testing.T) {
+	s := &[]Slice[int]{}
+	s.Append(1)
+	if ok := len(*s) == 1; !ok {
+		t.Fatalf("len(*Slice) != 1")
+	}
+	s := &[]Slice[int]{}
+	s.Append(1, 2)
+	if ok := len(*s) == 2; !ok {
+		t.Fatalf("len(*Slice) != 2")
+	}
+}
