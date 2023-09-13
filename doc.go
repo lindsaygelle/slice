a490fb42b0e90f9fb09ed7b86d85baac70239635
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-09-13 14:50:01 +1000

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,15 +1,2 @@
-// Package slice is a package of slice interfaces to handle common list-like operations.
-//
-// Slice contains a single Slice struct that exposes methods to perform traversal and mutation operations
-// for a collection of Go interfaces. The Slice struct can be extended to handle
-// the acceptance and selection of interface specific types. To extend the Slice an interface
-// can be defined that calls the exposed Slice methods.
-//
-// Package slice comes with all Go primative types as interfaces out of the box.
-//
-// Each slice interface comes with a constructor function that takes zero to n arguments of the
-// slice interface type.
-//
-// The slice interfaces to not expose the underlying interface slice to prevent a dirty reference.
-// This pattern should be adopted when wrapping the Slice struct.
+// Package slice provides a comprehensive set of list operations like appending, deleting, concatenating, mapping, reversing, and more, all seamlessly integrated into single generic slice.
 package slice
