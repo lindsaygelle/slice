98be21c8eac449b8d232464a6560209d6066fc6c
lindsaygelle <lindsay.gelle@gmail.com>
2023-09-13 21:30:31 +1000

    Dev

--- doc.go
+++ doc.go
@@ -1,2 +1,20 @@
-// Package slice provides a list-like struct with methods for traversal and mutation operations by numeric index.
+// Package slice: A versatile Go library for effortless slice management.
+//
+// Welcome to the Slice package, a powerful and versatile Go library designed to simplify and elevate your experience with slices in Go.
+// This package provides a generic slice implementation that goes beyond the basics, offering an extensive range of methods for traversal and mutation operations based on numeric indices.
+//
+// Why Slice?
+//
+// Slices are fundamental data structures in Go, but managing them efficiently can often be a complex and error-prone task.
+// The Slice package steps in to alleviate these challenges, offering a comprehensive set of utilities that make working with slices a breeze.
+//
+// # Key Features
+//
+// - Traverse with Confidence: Access and navigate your slices with ease using a rich set of traversal methods.
+// - Effortless Mutation: Seamlessly modify your slices by leveraging a variety of mutation operations based on numeric indices.
+// - Enhanced Functionality: Go beyond the basics with extended functionality that simplifies complex slice manipulations.
+//
+// # Package Documentation
+//
+// Explore the full range of features and functions available in the Slice package by referring to the comprehensive package documentation.
 package slice
