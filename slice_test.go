adbc5e1e079aa8f0fc90087c2db3ecd5b52f6c53
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-09-13 14:58:54 +1000

    Update slice_test.go

--- slice_test.go
+++ slice_test.go
@@ -8,12 +8,12 @@ import (
 
 // TestAppend tests Slice.Append.
 func TestAppend(t *testing.T) {
-	s := &[]Slice[int]{}
+	s := &slice.Slice[int]{}
 	s.Append(1)
 	if ok := len(*s) == 1; !ok {
 		t.Fatalf("len(*Slice) != 1")
 	}
-	s := &[]Slice[int]{}
+	s = &slice.Slice[int]{}
 	s.Append(1, 2)
 	if ok := len(*s) == 2; !ok {
 		t.Fatalf("len(*Slice) != 2")
