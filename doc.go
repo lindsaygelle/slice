a5273e97b1a04eb0cdc55c70dbc96c39bd878dad
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-10-20 16:48:30 +1100

    Dev (#48)
    * Dev

--- doc.go
+++ doc.go
@@ -1,3 +1,3 @@
 // Package slice is a Go package that offers a generic slice with extended functionality.
-// It abstracts common list operations, such as appending, deleting, concatenating, mapping, and more, making it easier to work with slices in Go.
+// It abstracts common list operations, such as appending, deleting, concatenating, filtering, mapping, and more, making it easier to work with slices in Go.
 package slice
--- example_test.go
+++ example_test.go
@@ -2,76 +2,36 @@ package slice_test
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/lindsaygelle/slice"
 )
 
 func ExampleNew() {
-	// Create a new Slice
-	s := slice.New(1, 2, 3, 4, 5)
+	// Creating a new integer slice
+	intSlice := slice.New[int](1, 2, 3, 4, 5)
 
-	// Append values to the Slice
-	s.Append(6, 7, 8)
-	fmt.Println("Appended:", s)
+	// Print the created slice
+	fmt.Println("Integer Slice:", intSlice)
 
-	// Append values based on a custom condition
-	s.AppendFunc(func(i int, value int) bool {
-		return value%2 == 0
-	}, 10, 11, 12)
-	fmt.Println("Appended based on condition:", s)
+	// Creating a new string slice
+	stringSlice := slice.New[string]("apple", "orange", "banana", "grape")
 
-	// Get the length of the Slice
-	fmt.Println("Length of Slice:", s.Length())
+	// Print the created slice
+	fmt.Println("String Slice:", stringSlice)
 
-	// Check if the Slice contains a specific value
-	fmt.Println("Contains 3:", s.Contains(3)) // Should print true
-	fmt.Println("Contains 9:", s.Contains(9)) // Should print false
+	// Creating a new http.Request slice
+	httpRequestSlice := slice.New[http.Request](http.Request{})
 
-	// Create a new Slice of booleans indicating if values are present in the original Slice
-	contains := s.ContainsMany(2, 5, 8, 10)
-	fmt.Println("Contains:", contains)
+	// Print the created slice
+	fmt.Println(httpRequestSlice)
 
-	// Delete an element at a specific index
-	s.Delete(2)
-	fmt.Println("Deleted at index 2:", s)
+	// Define a custom struct type
+	type Animal struct{ Name string }
 
-	// Iterate over the Slice and apply a function to each element
-	s.Each(func(i int, value int) {
-		fmt.Printf("Index %d: %d\n", i, value)
-	})
+	// Creating a custom slice
+	customSlice := slice.New[Animal](Animal{"Cat"}, Animal{"Dog"})
 
-	// Find the index of an element that satisfies a condition
-	index, found := s.FindIndex(func(value int) bool {
-		return value == 4
-	})
-	if found {
-		fmt.Println("Index of 4:", index) // Should print 3
-	} else {
-		fmt.Println("4 not found")
-	}
-
-	// Get an element at a specific index
-	value, ok := s.Get(1)
-	if ok {
-		fmt.Println("Value at index 1:", value) // Should print 2
-	} else {
-		fmt.Println("Index 1 not found")
-	}
-
-	// Make a new Slice with specific values
-	s = s.MakeEach(5, 4, 3, 2, 1)
-	fmt.Println("New Slice:", s)
-
-	// Reverse the Slice
-	s.Reverse()
-	fmt.Println("Reversed Slice:", s)
-
-	// Shuffle the Slice
-	s.Shuffle()
-	fmt.Println("Shuffled Slice:", s)
-
-	// Slice the Slice from index 2 to 5 (exclusive)
-	s.Slice(2, 5)
-	fmt.Println("Sliced Slice:", s)
-
+	// Print the created slice
+	fmt.Println(customSlice)
 }
